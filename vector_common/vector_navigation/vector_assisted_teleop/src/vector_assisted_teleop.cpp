ea679825cf19968484b29c44220e5872d2f1abf8
PatrickHussey <dev@stanleyinnovation.com>
2018-08-23 14:21:14 -0400

    fixes to turn on assisted teleop by default

--- vector_common/vector_navigation/vector_assisted_teleop/src/vector_assisted_teleop.cpp
+++ vector_common/vector_navigation/vector_assisted_teleop/src/vector_assisted_teleop.cpp
@@ -71,6 +71,8 @@ namespace assisted_teleop {
     cmd_vel_.linear.x = 0.0;
     cmd_vel_.linear.y = 0.0;
     cmd_vel_.linear.z = 0.0;
+    vel_cmd_has_updated1 = false;
+    vel_cmd_has_updated2 = false;
 
     planning_thread_ = new boost::thread(boost::bind(&AssistedTeleop::controlLoop, this));
   }
@@ -88,6 +90,9 @@ namespace assisted_teleop {
     {
         cmd_vel_.linear.y = 0.0;
     }
+    vel_cmd_has_updated1=true;
+    vel_cmd_has_updated2=true;
+    
   }
 
   void AssistedTeleop::controlLoop(){
@@ -109,7 +114,11 @@ namespace assisted_teleop {
         cmd.linear.x = desired_vel[0];
         cmd.linear.y = desired_vel[1];
         cmd.angular.z = desired_vel[2];
-        pub_.publish(cmd);
+        if (true == vel_cmd_has_updated1)
+        {
+            pub_.publish(cmd);
+            vel_cmd_has_updated1 = false;
+        }
         r.sleep();
         continue;
       }
@@ -208,8 +217,12 @@ namespace assisted_teleop {
       best_cmd.linear.x = best[0];
       best_cmd.linear.y = best[1];
       best_cmd.angular.z = best[2];
-      pub_.publish(best_cmd);
-
+      if (true == vel_cmd_has_updated2)
+      {
+        pub_.publish(best_cmd);
+        vel_cmd_has_updated2 = false;
+      }
+      
       r.sleep();
     }
   }
